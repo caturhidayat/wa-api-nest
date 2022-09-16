68760b0a66aeca9a74ec47855649cfacbe3447e0
caturhidayat <caturhidayat00@gmail.com>
2022-09-16 10:25:50 +0700

    add wasap resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WasapService } from './wasap/wasap.service';
+import { WasapController } from './wasap/wasap.controller';
+import { WasapModule } from './wasap/wasap.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WasapModule],
+  controllers: [AppController, WasapController],
+  providers: [AppService, WasapService],
 })
 export class AppModule {}
---
+++ src/wasap/wasap.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('wasap')
+export class WasapController {}
