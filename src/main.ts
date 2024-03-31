87a1f78fc154cde2337bd71373d0202d0df4d39b
Catur Hidayat <caturhidayat00@gmail.com>
2024-03-31 22:53:31 +0700

    docs(app): swagger add document API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('WA-API-NEST')
+    .setDescription('Birthday message automation API using NestJS')
+    .setVersion('1.0')
+    .addTag('message')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,31 +9,43 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, description: 'User created' })
   @Post()
-  create(@Body() userDto: UserDto) {
+  async create(@Body() userDto: UserDto) {
     return this.userService.create(userDto);
   }
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users' })
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiResponse({ status: 200, description: 'Return user by id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Update user by id' })
+  @ApiResponse({ status: 200, description: 'User updated' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() userDto: UserDto) {
     return this.userService.update(+id, userDto);
   }
 
+  @ApiOperation({ summary: 'Delete user by id' })
+  @ApiResponse({ status: 200, description: 'User deleted' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
